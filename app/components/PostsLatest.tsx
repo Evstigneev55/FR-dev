cf7650e66c66a669ca57801064071077d09022b4
Bethoven <moais2024@mail.ru>
2025-08-16 03:38:08 +0400

    1 v.

---
+++ app/components/PostsLatest.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router';
+import type { PostMeta } from '~/routes/types/post';
+
+const PostsLatest = ({ posts, limit = 2 }: PostsLatestProps) => {
+	const latests = [...posts].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()).slice(0, limit);
+
+	return (
+		<section className='max-w-6xl mx-auto px-6 py-12'>
+			<h2 className='text-2xl font-bold mb-6 text-white'>🆕 Latests posts</h2>
+
+			<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+				{latests.map((post) => (
+					<Link
+						key={post.slug}
+						to={`blog/${post.slug}`}
+						className='block p-4 border border-gray-700 rounded-lg bg-gray-800 hover:shadow-md transition'
+					>
+						<h3 className='text-lg text-semibold text-blue-400 mb-1'>{post.title}</h3>
+						<p className='text-s text-gray-300'>{post.excerpt}</p>
+						<span className='block mt-2 text-xs text-gray-400'>{new Date(post.date).toDateString()}</span>
+					</Link>
+				))}
+			</div>
+		</section>
+	);
+};
+export default PostsLatest;
+
+interface PostsLatestProps {
+	posts: PostMeta[];
+	limit?: number;
+}
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -2,26 +2,33 @@ import FeaturedProjects from '~/components/FeaturedProjects';
 import type { Route } from './+types/index';
 import type { Project } from '../types/project';
 import AboutPreview from '~/components/AboutPreview';
+import type { PostMeta } from '../types/post';
+import PostsLatest from '~/components/PostsLatest';
 
 export function meta({}: Route.MetaArgs) {
 	return [{ title: 'The Friendly Dev | Welcome' }, { name: 'description', content: 'Welcome to the main page!' }];
 }
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-	const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/projects`);
-	if (!res.ok) throw new Response('Failed to fetch products', { status: 400 });
-	const data: Project[] = await res.json();
-
-	return { projects: data };
+//! Change: Promise.all()  To  Promise.allSettled()
+export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+	const url = new URL(request.url);
+	const [resProjects, resPosts] = await Promise.all([
+		fetch(`${import.meta.env.VITE_API_BASE_URL}/projects`),
+		fetch(new URL('/posts-meta.json', url)),
+	]);
+	if (!resProjects.ok || !resPosts.ok) throw new Error('Failed to fetch posts or projects');
+	const [projects, posts] = await Promise.all([resProjects.json(), resPosts.json()]);
+	return { projects, posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
-	const { projects } = loaderData;
+	const { projects, posts } = loaderData;
 
 	return (
 		<>
-			<FeaturedProjects projects={projects} count={2}/>
+			<FeaturedProjects projects={projects} count={2} />
 			<AboutPreview />
+			<PostsLatest posts={posts} limit={3}/>
 		</>
 	);
 };
